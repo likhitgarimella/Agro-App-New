e1a08f803145d3d546bac51859865cb8c95036a9
Likhit <likhitgmella@gmail.com>
2020-02-28 03:31:01 +0530

    First Commit

---
+++ Agro/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Agro
+//
+//  Created by Likhit Garimella on 22/02/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import Foundation
+
+let temp = "Something"
